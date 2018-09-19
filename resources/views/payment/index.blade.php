a4149e3e1ef378a2430aa096af895452d20734e3
Frédérick Tyteca <frederick@tyteca.net>
2018-09-19 22:26:08 +0200

    Stripe - formulaire is sending a request on SubscribeController (dd only for now)

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -1,16 +1,31 @@
+@extends('layouts.app')
+
+@section('pageTitle', __('messages.page_title_channel_thumbs_index') )
+
+@section ('content')
+
+    <div class="container text-center">
+      
+      <form action="/subscribe" method="POST">
+
+        {{ csrf_field() }}
+
+        <script
+        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+        data-key="{{ env('STRIPE_KEY') }}"
+        data-amount="900"
+        data-name="Podmytube"
+        data-description="Weekly Youtuber 9€"
+        data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+        data-locale="auto"
+        date-currency="EUR"
+        data-zip-code="true">
+        </script>
+      </form>
+
+    </div>
+
+@endsection
 
 
 
-
-<form action="/purchase" method="POST">
-  <script
-    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-    data-key="{{ config('services.stripe.key') }}"
-    data-amount="999"
-    data-name="Stripe.com"
-    data-description="Widget"
-    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-    data-locale="auto"
-    data-zip-code="true">
-  </script>
-</form>
