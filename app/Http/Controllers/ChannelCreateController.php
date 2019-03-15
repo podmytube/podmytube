9c092493870e257857f3d6497b9070ccb628a443
Frédérick Tyteca <frederick@tyteca.net>
2019-03-15 21:52:15 +0100

    WIP - exporting channel creation from controller to service

--- app/Http/Controllers/ChannelCreateController.php
+++ app/Http/Controllers/ChannelCreateController.php
@@ -10,9 +10,10 @@
  */
 namespace App\Http\Controllers;
 use Auth;
-use App\Channel;
 use App\User;
+use App\Channel;
 use App\Mail\ChannelIsRegistered;
+use App\Services\CreateChannelService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Mail;
@@ -34,53 +35,20 @@ class ChannelCreateController extends Controller
      * create one channel from the form received
      *
      * @return void
-     * @todo remove youtube_channel_id USELESS but mandatory
      */
     public function store(Request $request)
     {
-        /**
-         * The only field required is the channel id. We are asking for the url channel
-         * It should be 26 characters long too contain at least http://youtube.com/channel/
-         */
-        $this->validate(request(), [
-
-            'channel_url' => 'required|string|min:27'
-
-        ]);
-
-        if (!$channel_id = Channel::extractChannelIdFromUrl(request('channel_url'))) {
-            return Redirect::back()->withErrors([__('messages.flash_channel_id_is_invalid')]);
-        }
-
-        /**
-         * Channel creating
-         */
-        $channel = Channel::create([
-            'user_id' => \Auth::user()->user_id,
-
-            'channel_id' => $channel_id,
-
-            'channel_name' => __('messages.channel_to_be_validated'),
-
-            'youtube_channel_id' => $channel_id,
-
-        ]);
-        $channel->subscription->create(request()->all());
-
-        /**
-         * Getting current authenticated user
-         */
-        $user = User::find(\Auth::user()->user_id);
-        
-        /**
-         * Sending congratulations mail 
-         */
-        Mail::to($user)->send(new ChannelIsRegistered($user, $channel));
+        try {
+            CreateChannelService::create($request);
+        } catch (\Exception $e) {
+            session()->flash('message', __('messages.creation_channel_has_failed'));
+            session()->flash('messageClass', 'alert-info');
+        }        
 
         /**
          * Displaying congratulations and redirect 
          */
-        request()->session()->flash('status', __('messages.flash_channel_has_been_created'));
+        session()->flash('status', __('messages.flash_channel_has_been_created'));
         return redirect('/home');
     }
 }
---
+++ app/Services/CreateChannelService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+
+use App\Channel;
+use App\user;
+/**
+ * This class is used when a channel is created
+ */
+class CreateChannelService
+{
+    public function create(Request $request)
+    {
+        /**
+         * The only field required is the channel id. We are asking for the url channel
+         * It should be 26 characters long too contain at least http://youtube.com/channel/
+         */
+        $this->validate(request(), [
+
+            'channel_url' => 'required|string|min:27',
+
+        ]);
+
+        if (!$channel_id = Channel::extractChannelIdFromUrl(request('channel_url'))) {
+            return Redirect::back()->withErrors([__('messages.flash_channel_id_is_invalid')]);
+        }
+
+        /**
+         * Channel creating
+         */
+        $channel = Channel::create([
+            'user_id' => \Auth::user()->user_id,
+
+            'channel_id' => $channel_id,
+
+            'channel_name' => __('messages.channel_to_be_validated'),
+
+            'youtube_channel_id' => $channel_id,
+
+        ]);
+        $channel->subscription->create(request()->all());
+
+        /**
+         * Getting current authenticated user
+         */
+        $user = User::find(\Auth::user()->user_id);
+
+        /**
+         * Sending congratulations mail
+         */
+        Mail::to($user)->send(new ChannelIsRegistered($user, $channel));
+    }
+}
