bcdf6dadabcda556f6af81b156eae923177757b3
Frederick Tyteca <frederick@tyteca.net>
2019-04-01 14:00:30 +0200

    adding publiched between scope

--- app/Medias.php
+++ app/Medias.php
@@ -41,4 +41,28 @@ class Medias extends Model
             ]
         );
     }
+
+    /**
+     * define a scope to get medias published between 2 dates.
+     * @param object query is the query object
+     * @param array value should have 2 date in it [0] is the startDate, [1] is the endDate
+     */
+    public function scopePublishedBetween(
+        $query,
+        Carbon $startDate,
+        Carbon $endDate
+    ) {
+
+        if ($startDate > $endDate) {
+            throw new \Exception("Start date should be before end date !");
+        }
+
+        return $query->whereBetween(
+            'published_at',
+            [
+                $startDate->toDateString(),
+                $endDate->toDateString()
+            ]
+        );
+    }
 }
