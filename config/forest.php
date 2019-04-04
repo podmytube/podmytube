f7fe6433777598d46d2bf582a0e796c6ed961110
Frederick Tyteca <frederick@tyteca.net>
2019-04-04 14:22:27 +0200

    adding debug mode to forest

--- config/forest.php
+++ config/forest.php
@@ -3,5 +3,6 @@
 return [
     'secret_key' => env('FOREST_ENV_SECRET'),
     'auth_key' => env('FOREST_AUTH_SECRET'),
-    'models_path' => 'app'
+    'models_path' => 'app',
+    'debug_mode' => true,
 ];
