c32477885c6ab3029a94f2d571332fc67073b1b1
frederick tyteca <frederick@tyteca.net>
2019-05-06 23:09:25 +0200

    WIP

--- app/Http/Controllers/ChannelsController.php
+++ app/Http/Controllers/ChannelsController.php
@@ -8,89 +8,92 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Auth;
+use App\Channel;
 use App\Http\Requests\ChannelRequest;
 use App\Services\ChannelService;
-
-use App\Channel;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * the channel controller class.
  */
 class ChannelsController extends Controller
 {
-	/**
-	 * mainly useful to guard some routes
-	 */
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+    /**
+     * mainly useful to guard some routes
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-	/**
-	 * get list of user's channel
-	 * @return Response
-	 */
-	public function index()
-	{
-		$channels = ChannelService::getAuthenticatedUserChannels(Auth::user());
-
+    /**
+     * get list of user's channel
+     * @return Response
+     */
+    public function index()
+    {
+        try {
+            $channels = ChannelService::getAuthenticatedUserChannels(Auth::user());
+        } catch (\Exception $e) {
+			/**
+			 * @TODO adding a message to add one channel
+			 */
+			1;
+			$channels=[];
+		}
         return view('channel.index', compact('channels'));
+    }
 
-	}
+    /**
+     * display all informations about one channel
+     *
+     * @param  ChannelRequest      $request
+     * @param  Channel          $channel
+     * @return Response*
+     */
 
-	/**
-	 * display all informations about one channel
-	 * 
-	 * @param  ChannelRequest  	$request 
-	 * @param  Channel  		$channel
-	 * @return Response*
-	 */
+    public function show(ChannelRequest $request, Channel $channel)
+    {
 
-	public function show(ChannelRequest $request, Channel $channel)
-	{
+        $channel->podcast_url = \App\Channel::podcastUrl($channel);
+        $channel->youtube_url = \App\Channel::youtubeUrl($channel);
+        //dd($channel->channel_id);
 
-		$channel->podcast_url = \App\Channel::podcastUrl($channel);
-		$channel->youtube_url = \App\Channel::youtubeUrl($channel);
-		//dd($channel->channel_id);
+        return view('channel.show', compact('channel'));
 
-		return view('channel.show', compact('channel'));
+    }
 
-	}
+    /**
+     * display the channel form in order to edit channel data
+     *
+     * @param  ChannelRequest          $request
+     * @param  Channel              $channel
+     * @return Response
+     */
+    public function edit(ChannelRequest $request, Channel $channel)
+    {
 
-	/**
-	 * display the channel form in order to edit channel data
-	 *
-	 * @param  ChannelRequest          $request
-	 * @param  Channel              $channel
-	 * @return Response
-	 */
-	public function edit(ChannelRequest $request, Channel $channel)
-	{
+        return view('channel.edit', compact('channel'));
 
-		return view('channel.edit', compact('channel'));
+    }
 
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  ChannelRequest $request the form request
+     * @param  Channel $channel channel concerned
+     * @return Response
+     */
+    public function update(ChannelRequest $request, Channel $channel)
+    {
+        //$channel = Channel::findOrFail($id);
 
+        $channel->update($request->all());
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  ChannelRequest $request the form request 
-	 * @param  Channel $channel channel concerned
-	 * @return Response
-	 */
-	public function update(ChannelRequest $request, Channel $channel)
-	{
-		//$channel = Channel::findOrFail($id);
+        \Session::flash('message', 'Channel successfully updated !');
+        \Session::flash('alert-class', 'alert-success');
 
-		$channel->update($request->all());
+        return redirect('channel/' . $channel->channel_id);
 
-		\Session::flash('message', 'Channel successfully updated !');
-		\Session::flash('alert-class', 'alert-success');
-
-		return redirect('channel/' . $channel->channel_id);
-
-	}
+    }
 }
