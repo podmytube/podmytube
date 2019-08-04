da967c344a86add9f11c643b926a7ebc35d6c6c6
frederick tyteca <frederick@tyteca.net>
2019-08-04 11:29:34 +0200

    wip : testing category model with relation ship

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //protected $table = 'Categor';
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    //public function childrenRecursive()
+    //{
+    //    return $this->children()->with('childrenRecursive');
+    //}
 }
