b597a2346fee8d65fc603fba5505ffee7a17b42e
Frederick Tyteca <frederick@tyteca.net>
2019-08-12 09:32:14 +0200

    wip

--- tests/Unit/CategoryModelTest.php
+++ tests/Unit/CategoryModelTest.php
@@ -3,21 +3,72 @@
 namespace Tests\Unit;
 
 use App\Category;
+use illuminate\Support\Collection;
 use Tests\TestCase;
 
 class CategoryModelTest extends TestCase
 {
+    protected $artsCategoryChildren;
+
+    public function setUp(): void
+    {
+        $this->artsCategoryChildren = collect([
+            ["id" => 20, "name" => "books"],
+            ["id" => 21, "name" => "design"],
+            ["id" => 22, "name" => "fashionAndBeauty"],
+            ["id" => 23, "name" => "food"],
+            ["id" => 24, "name" => "performingArts"],
+            ["id" => 25, "name" => "visualArts"],
+        ]);
+        parent::setUp();
+    }
+
+    public function testSimpleParentRelationShipShouldBeOk()
+    {
+        $expectedCategory = Category::find(1);
+        $this->assertEquals(
+            $expectedCategory->name,
+            Category::find(23)->parent->name
+        );
+    }
+
+    public function testGetCategoryByUnknownNameShouldReturnNullResult()
+    {
+        $this->assertNull(Category::byName("This category will never exist"));
+    }
+
+    public function testGetCategoryByNameShouldBeValid()
+    {
+        $results = Category::byName("arts");
+        $expectedCategoryId = 1;
+        $this->assertEquals(
+            $expectedCategoryId,
+            $results->id
+        );
+
+        $this->assertEqualsCanonicalizing(
+            $this->artsCategoryChildren->pluck('name'),
+            $results->children->pluck('name')
+        );
+    }
+
     public function testThatIGetAllCategories()
     {
-        $expectedArtsCategories = [
-
-        ];
-        $results = Category::find(1)->with('children')->get();
-        foreach ($results as $parentCategory){
-            
-
-            
-        }
+        $expectedCategories = collect([
+            "id" => 1,
+            "name" => "arts",
+            "children" => $this->artsCategoryChildren
+        ]);
+        $results = Category::find(1)->with('children')->first();
+        $this->assertInstanceOf(Category::class, $results);
+        $this->assertEquals($expectedCategories->get('id'), $results->id);
+        $this->assertEquals($expectedCategories->get('name'), $results->name);
+        $this->assertEqualsCanonicalizing(
+            $expectedCategories->get('children')->pluck('id'),
+            $results->children->map(function ($child) {
+                return $child->id;
+            }) // similar to $results->children->pluck('id')
+        );
     }
 
     public function testThatIGetRightParentId()
