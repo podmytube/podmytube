1f761e808c228a8d7cd5996a43c00f4c55f150db
frederick tyteca <frederick@tyteca.net>
2019-08-13 15:22:25 +0200

    categories tests are valids

--- tests/Unit/CategoryModelTest.php
+++ tests/Unit/CategoryModelTest.php
@@ -3,55 +3,107 @@
 namespace Tests\Unit;
 
 use App\Category;
-use illuminate\Support\Collection;
 use Tests\TestCase;
+
+use App\Providers\AppServiceProvider;
 
 class CategoryModelTest extends TestCase
 {
-    protected static $artsCategoryId = 1;
-    protected static $artsCategoryChildren;
-    protected static $musicCategoryId = 11;
-    protected static $musicCategoryChildren;
+    protected static $expectedCategories;
+    protected static $expectedTotalNumberOfCategories = 107;
+    protected static $expectedNumberOfParentCategories = 19;
+    protected static $categoriesWithoutChildren = [
+        "comedy" => ["id" => 3],
+        "government" => ["id" => 6],
+        "history" => ["id" => 7],
+        "technology" => ["id" => 17],
+        "trueCrime" => ["id" => 18],
+    ];
 
     public function setUp(): void
     {
-        self::$artsCategoryChildren = collect([
-            ["id" => 20, "name" => "books"],
-            ["id" => 21, "name" => "design"],
-            ["id" => 22, "name" => "fashionAndBeauty"],
-            ["id" => 23, "name" => "food"],
-            ["id" => 24, "name" => "performingArts"],
-            ["id" => 25, "name" => "visualArts"],
-        ]);
+        /**
+         * small extract of the available categories
+         */
+        self::$expectedCategories = [
+            "arts" => [
+                'id' => 1,
+                'name' => 'arts',
+                "children" => [
+                    ["id" => 20, "name" => "books"],
+                    ["id" => 21, "name" => "design"],
+                    ["id" => 22, "name" => "fashionAndBeauty"],
+                    ["id" => 23, "name" => "food"],
+                    ["id" => 24, "name" => "performingArts"],
+                    ["id" => 25, "name" => "visualArts"],
+                ]
+            ],
+            "government" => [
+                'id' => 6,
+                'name' => 'government',
+            ],
+            "music" => [
+                'id' => 11,
+                'name' => 'music',
+                "children" => [
+                    ["id" => 57, "name" => "musicCommentary"],
+                    ["id" => 58, "name" => "musicHistory"],
+                    ["id" => 59, "name" => "musicInterviews"],
+                ]
+            ]
+        ];
 
-        self::$musicCategoryChildren = collect([
-            ["id" => 57, "name" => "musicCommentary"],
-            ["id" => 58, "name" => "musicHistory"],
-            ["id" => 59, "name" => "musicInterviews"],
-        ]);
+        //dd(self::$expectedCategories->contains("name", "government"));
 
         parent::setUp();
     }
 
+    public function testingSomeNumberFromCategories()
+    {
+        /**
+         * Checking we have the right number of categories and parent categories.
+         */
+        $this->assertEquals(self::$expectedTotalNumberOfCategories, Category::all()->count());
+        $this->assertEquals(self::$expectedNumberOfParentCategories, Category::list()->count());
+    }
+
     public function testListingCategories()
     {
-        $expectedCategory = Category::find(self::$artsCategoryId);
-        $categoriesList = Category::list();
-        $wantedCategories = [self::$artsCategoryId, self::$musicCategoryId];
-        
-        $results = $categoriesList->filter(function ($item) use ($wantedCategories) {
-            if (in_array($item->id, $wantedCategories)) {
-                return $item;
+        $results = Category::list();
+        foreach (self::$expectedCategories as $expectedCategory) {
+            $result = $results->firstWhere("id", $expectedCategory["id"]);
+            $this->assertEquals($expectedCategory["id"], $result->id);
+            $this->assertEquals($expectedCategory["name"], $result->name);
+
+            if (!isset($expectedCategory['children'])) {
+                /**
+                 * some categories have no have no children (poors :'( ))
+                 */
+                $this->assertTrue(
+                    in_array(
+                        $result->name,
+                        array_keys(self::$categoriesWithoutChildren)
+                    )
+                );
+                continue;
             }
-        });
 
-        dd($results->pluck('name'));
+            /**
+             * Checking names
+             */
+            $this->assertEqualsCanonicalizing(
+                collect($expectedCategory['children'])->pluck("name")->toArray(),
+                $result->children->pluck('name')->toArray()
+            );
 
-        $this->assertEqualsCanonicalizing(
-            self::$artsCategoryChildren->pluck('name'),
-            $results->children->pluck('name')
-        );
-        
+            /**
+             * Checking ids
+             */
+            $this->assertEqualsCanonicalizing(
+                collect($expectedCategory['children'])->pluck("id")->toArray(),
+                $result->children->pluck('id')->toArray()
+            );
+        }
     }
 
     /**
@@ -60,7 +112,7 @@ class CategoryModelTest extends TestCase
      */
     public function testSimpleParentRelationShipShouldBeOk()
     {
-        $expectedCategory = Category::find(self::$artsCategoryId);
+        $expectedCategory = Category::find(self::$expectedCategories["arts"]["id"]);
         $this->assertEquals(
             $expectedCategory->name,
             Category::find(23)->parent->name
@@ -75,47 +127,45 @@ class CategoryModelTest extends TestCase
     public function testGetCategoryByNameShouldBeValid()
     {
         $results = Category::byName("arts");
-        $expectedCategoryId = self::$artsCategoryId;
+        $expectedCategoryId = self::$expectedCategories["arts"]["id"];
         $this->assertEquals(
             $expectedCategoryId,
             $results->id
         );
 
         $this->assertEqualsCanonicalizing(
-            self::$artsCategoryChildren->pluck('name'),
+            collect(self::$expectedCategories["arts"]["children"])->pluck('name'),
             $results->children->pluck('name')
         );
     }
 
     public function testThatIGetAllCategories()
     {
-        $expectedCategories = collect([
-            "id" => self::$artsCategoryId,
-            "name" => "arts",
-            "children" => self::$artsCategoryChildren
-        ]);
         $results = Category::find(1)->with('children')->first();
         $this->assertInstanceOf(Category::class, $results);
-        $this->assertEquals($expectedCategories->get('id'), $results->id);
-        $this->assertEquals($expectedCategories->get('name'), $results->name);
+        $this->assertEquals(self::$expectedCategories["arts"]['id'], $results->id);
+        $this->assertEquals(self::$expectedCategories["arts"]['name'], $results->name);
         $this->assertEqualsCanonicalizing(
-            $expectedCategories->get('children')->pluck('id'),
+            collect(self::$expectedCategories["arts"]["children"])->pluck('id'),
             $results->children->map(function ($child) {
                 return $child->id;
             }) // similar to $results->children->pluck('id')
         );
     }
 
+    /**
+     * Checking that the parent id of one art category is the id of the art category itself.
+     */
     public function testThatIGetRightParentId()
     {
-        $expectedParentCategory = Category::where('name', 'arts')->first();
         $artsSubCategories = range(20, 25);
         foreach ($artsSubCategories as $subCategoryId) {
             $subCategory = Category::find($subCategoryId);
             $this->assertEquals(
-                $expectedParentCategory->id,
+                self::$expectedCategories["arts"]["id"],
                 $subCategory->parent_id,
-                "Category id for {{$subCategory->name}} should be {{$expectedParentCategory->id}} ({{$expectedParentCategory->name}})"
+                "Category id for {{$subCategory->name}} should be {" . self::$expectedCategories["arts"]["id"] . "} " .
+                    "was ({" . $subCategory->name . "})"
             );
         }
     }
@@ -133,9 +183,10 @@ class CategoryModelTest extends TestCase
     public function testThatIGotArtsCategoryIdProperly()
     {
         $this->assertEquals(
-            1,
+            self::$expectedCategories["arts"]['id'],
             Category::where('name', 'arts')->first()->id,
-            "Arts category should be {{" . self::$artsCategoryId . "}}, was {{Category::where('name','arts')->first()->id}}"
+            "Arts category should be {{" . self::$expectedCategories["arts"]['id'] . "}}," .
+                "was {{" . Category::where('name', 'arts')->first()->id . "}}"
         );
     }
 }
