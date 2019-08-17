916ec9e9aed805b7d7c2c053f327f212269cf712
frederick tyteca <frederick@tyteca.net>
2019-08-18 00:45:57 +0200

    wip

---
+++ resources/lang/fr/categories.php
@@ -0,0 +1,204 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Categories
+    |--------------------------------------------------------------------------
+    |
+    | Apple categories labels
+    |
+    */
+    "select_your_category" => "Choix d'une catégorie",
+
+    /**
+     * Parent categories
+     */
+    'arts' => 'Arts',
+    'business' => 'Business',
+    'comedy' => 'Comedie',
+    'education' => 'Education',
+    'fiction' => 'Fiction',
+    'government' => 'Gouvernment',
+    'history' => 'Histoire',
+    'healthFitness' => 'Health & Fitness', // htmlentities will tranform it into "Health &amp; Fitness" as wanted by Apple
+    'kidsFamily' => 'Enfance & Famille',
+    'leisure' => 'Loisirs',
+    'music' => 'Musique',
+    'news' => 'News',
+    'religionSpirituality' => 'Religion & Spiritualité',
+    'science' => 'Science',
+    'societyCulture' => 'Société & Culture',
+    'sports' => 'Sports',
+    'technology' => 'Technologie',
+    'trueCrime' => 'Crime Véritable',
+    'tvFilm' => 'TV & Film',
+
+    /**
+     * Arts
+     */
+    "books" => 'Books',
+    "design" => 'Design',
+    "fashionAndBeauty" => 'Fashion & Beauty',
+    "food" => 'Food',
+    "performingArts" => 'Performing Arts',
+    "visualArts" => 'Visual Arts',
+
+    /**
+     * Business
+     */
+    "careers" => "Careers",
+    "entrepreneurShip" => "Entrepreneurship",
+    "investing" => "Investing",
+    "management" => "Management",
+    "marketing" => "Marketing",
+    "nonProfit" => "NonProfit",
+
+    /**
+     * Comedy
+     */
+    "comedyInterviews" => "Comedy Interviews",
+    "improv" => "Improv",
+    "standUp" => "Stand-Up",
+
+    /**
+     * Education
+     */    
+    "courses" => "Courses",
+    "howTo" => "How To",
+    "languageLearning" => "Language Learning",
+    "selfImprovement" => "Self-Improvement",
+
+    /**
+     * Fiction
+     */
+    "comedyFiction" => "Comedy Fiction",
+    "drama" => "Drama",
+    "scienceFiction" => "Science Fiction",
+
+    /**
+     * Government is Alone
+     */
+
+    /**
+     * History is alone
+     */
+
+    /**
+     * Health & Fitness
+     */
+    "alternativeHealth" => "Alternative Health",
+    "fitness" => "Fitness",
+    "medicine" => "Medicine",
+    "mentalHealth" => "Mental Health",
+    "nutrition" => "Nutrition",
+    "sexuality" => "Sexuality",
+
+    /**
+     * Kids & Family
+     */
+    "educationForKids" => "Education for Kids",
+    "parenting" => "Parenting",
+    "petsAnimals" => "Pets & Animals",
+    "storiesForKids" => "Stories For Kids",
+
+    /**
+     * Leisure
+     */
+    "animationManga" => "Animation & Manga",
+    "automotive" => "Automotive",
+    "aviation" => "Aviation",
+    "crafts" => "Crafts",
+    "games" => "Games",
+    "hobbies" => "Hobbies",
+    "homeGarden" => "Home & Garden",
+    "videoGames" => "Video Games",
+
+    /**
+     * Music
+     */
+    "musicCommentary" => "Music Commentary",
+    "musicHistory" => "Music History",
+    "musicInterviews" => "Music Interviews",
+
+    /**
+     * News
+     */
+    "businessNews" => "Business News",
+    "dailyNews" => "Daily News",
+    "entertainmentNews" => "Entertainment News",
+    "newsCommentary" => "News Commentary",
+    "politics" => "Politics",
+    "sportsNews" => "Sports News",
+    "techNews" => "Tech News",
+
+    /**
+     * Religion & Spirituality
+     */
+    "buddhism" => "Buddhism",
+    "christianity" => "Christianity",
+    "hinduism" => "Hinduism",
+    "islam" => "Islam",
+    "judaism" => "Judaism",
+    "religion" => "Religion",
+    "spirituality" => "Spirituality",
+
+    /**
+     * Science
+     */
+    "astronomy" => "Astronomy",
+    "chemistry" => "Chemistry",
+    "earthSciences" => "Earth Sciences",
+    "lifeSciences" => "Life Sciences",
+    "mathematics" => "Mathematics",
+    "naturalSciences" => "Natural Sciences",
+    "nature" => "Nature",
+    "physics" => "Physics",
+    "socialSciences" => "Social Sciences",
+
+    /**
+     * Society & Culture
+     */
+    "documentary" => "Documentary",
+    "personalJournals" => "Personal Journals",
+    "philosophy" => "Philosophy",
+    "placesTravel" => "Places & Travel",
+    "relationships" => "Relationships",
+
+    /**
+     * Sports
+     */
+    "baseball" => "Baseball",
+    "basketball" => "Basketball",
+    "cricket" => "Cricket",
+    "fantasySports" => "Fantasy Sports",
+    "football" => "Football",
+    "golf" => "Golf",
+    "hockey" => "Hockey",
+    "rugby" => "Rugby",
+    "running" => "Running",
+    "soccer" => "Soccer",
+    "swimming" => "Swimming",
+    "tennis" => "Tennis",
+    "volleyball" => "Volleyball",
+    "wilderness" => "Wilderness",
+    "wrestling" => "Wrestling",
+
+    /**
+     * Technology is alone
+     */
+
+    /**
+     * TrueCrime is alone
+     */
+
+    /**
+     * Tv & Film
+     */
+    "afterShows" => "After Shows",
+    "filmHistory" => "Film History",
+    "filmInterviews" => "Film Interviews",
+    "filmReviews" => "Film Reviews",
+    "tvReviews" => "Tv Reviews",
+];
