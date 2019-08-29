8c9b150419f6927f08bd1a161555fb5337075afd
frederick tyteca <frederick@tyteca.net>
2019-08-29 09:40:31 +0200

    fixing category migration with empty/unknown category

--- app/Services/CategoryMigrationService.php
+++ app/Services/CategoryMigrationService.php
@@ -9,6 +9,8 @@ namespace App\Services;
 
 use App\Category;
 use App\Channel;
+use App\Exceptions\CategoryIsEmptyException;
+use App\Exceptions\CategoryIsUnknownException;
 
 /**
  * This class will be used (for a short time) to convert old
@@ -21,14 +23,18 @@ class CategoryMigrationService
      * This function will create one channelCategories model for one channel according to its category.
      *
      * @param object App\Channel model $channel the channel to convert
-     * @return void
+     * @return boolean mainly for tests
      */
     public static function transform(Channel $channel)
     {
+        if (!isset($channel->category) || empty($channel->category)) { 
+            throw new CategoryIsEmptyException("This category {{$channel->category}} is unknown.");
+        }
         try {
             /**
              * Getting category that fit to the old one
              */
+
             $newCategoryId = self::getVersion2019Category($channel->category);
 
             /**
@@ -36,7 +42,6 @@ class CategoryMigrationService
              */
             $channel->category_id = $newCategoryId;
             $channel->save();
-
         } catch (\Exception $e) {
             throw $e;
         }
@@ -96,7 +101,7 @@ class CategoryMigrationService
                 $categoryId = Category::where("name", "sports")->first()->id;
                 break;
             default:
-                throw new \Exception("This category {{$previousCategory}} is unknown.");
+                throw new CategoryIsUnknownException("This category {{$previousCategory}} is unknown.");
         }
 
         return $categoryId;
