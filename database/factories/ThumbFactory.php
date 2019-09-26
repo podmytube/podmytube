a1c57aaf78055fcad1f4965dfd0f44572f6013d9
frederick tyteca <frederick@tyteca.net>
2019-09-26 23:13:41 +0200

    wip

---
+++ database/factories/ThumbFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Thumb;
+use Faker\Generator as Faker;
+
+$factory->define(Thumb::class, function (Faker $faker) {
+    $channelId = $faker->regexify('[a-zA-Z0-9-_]{24}');
+    /**
+     * generate an image using loremPixels and storing it into
+     * /app/tmp. I need to use basename because filename is something
+     * like /app/tmp/07b38eca6266b5cad874444e429c7579.jpg
+     * and when I check the size, the storage disk (set in 
+     * filesystems.php) is already giving the path.
+     */
+    $fileName = basename(
+        $faker->image("/app/tmp", 1400, 1400, 'nature')
+    );
+
+    return [
+        'channel_id' => $channelId,
+        'file_name' => $fileName,
+        'file_disk' => Thumb::_TEMP_STORAGE_DISK, // where it is stored
+        'file_size' => Storage::disk(Thumb::_TEMP_STORAGE_DISK)->size("$fileName"), // its size
+    ];    
+});
