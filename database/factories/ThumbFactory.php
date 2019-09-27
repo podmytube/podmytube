0b01bddc2138af7758a728005807f6180a513f11
frederick tyteca <frederick@tyteca.net>
2019-09-27 18:00:29 +0200

    playing with factories ans still trying to test thumb sftp move

--- database/factories/ThumbFactory.php
+++ database/factories/ThumbFactory.php
@@ -2,11 +2,21 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
+use App\Channel;
 use App\Thumb;
 use Faker\Generator as Faker;
 
 $factory->define(Thumb::class, function (Faker $faker) {
-    $channelId = $faker->regexify('[a-zA-Z0-9-_]{24}');
+    /**
+     * creating fake channel (to have one)
+     */
+    factory(Channel::class)->create();
+
+    /**
+     * Getting one random channel
+     */
+    $channel=Channel::all()->random();
+    
     /**
      * generate an image using loremPixels and storing it into
      * /app/tmp. I need to use basename because filename is something
@@ -19,7 +29,7 @@ $factory->define(Thumb::class, function (Faker $faker) {
     );
 
     return [
-        'channel_id' => $channelId,
+        'channel_id' => $channel->channel_id,
         'file_name' => $fileName,
         'file_disk' => Thumb::_TEMP_STORAGE_DISK, // where it is stored
         'file_size' => Storage::disk(Thumb::_TEMP_STORAGE_DISK)->size("$fileName"), // its size
