45cccd6b29df4451acb5b0e5e195f08a99297071
frederick tyteca <frederick@tyteca.net>
2019-10-18 21:08:43 +0200

    channel name is a sentence and not a regexp now

--- database/factories/ChannelFactory.php
+++ database/factories/ChannelFactory.php
@@ -5,7 +5,7 @@
 $factory->define(App\Channel::class, function ($faker) {
     return [
         'channel_id' => $faker->regexify('[a-zA-Z0-9-_]{24}'),
-        'channel_name' => $faker->regexify('[a-zA-Z0-9-_]{24}'),
+        'channel_name' => $faker->sentence($nbWords = "3", $variableNbWords = true),
         'authors' => $faker->name,
         'email' => $faker->safeEmail,
         'description' => $faker->text(300),
