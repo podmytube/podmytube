d8506ed185e582c4cf7db876689db2efc8552e3e
frederick tyteca <frederick@tyteca.net>
2019-10-18 09:44:51 +0200

    if authenticated user we are setting his language preference as locale

--- app/Http/Middleware/Locale.php
+++ app/Http/Middleware/Locale.php
@@ -6,6 +6,7 @@ use Closure;
 use App;
 use Config;
 use Session;
+use Illuminate\Support\Facades\Auth;
 
 class Locale
 {
@@ -18,11 +19,22 @@ class Locale
 	 */
 	public function handle($request, Closure $next)
 	{
+		/** if user authenticated and had a language set, we use it */
+		if (Auth::check()) {
+			if (isset(Auth::user()->language) && in_array(Auth::user()->language, Config::get('app.locales'))) {
+				$locale = Auth::user()->language;
+				App::setLocale($locale);
+				return $next($request);
+			}
+		}
+		
 		$raw_locale = Session::get('locale');
 		if (in_array($raw_locale, Config::get('app.locales'))) {
 			$locale = $raw_locale;
+		} else {
+			$locale = Config::get('app.locale');
 		}
-		else $locale = Config::get('app.locale');
+
 		App::setLocale($locale);
 		return $next($request);
 	}
