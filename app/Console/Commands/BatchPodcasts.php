43c073b86c9831022393bb1cd3ff3508410a67e6
frederick tyteca <frederick@tyteca.net>
2020-01-21 22:35:18 +0100

    channels extractors, tests for them and subscriptionFactory

--- app/Console/Commands/BatchPodcasts.php
+++ app/Console/Commands/BatchPodcasts.php
@@ -3,27 +3,29 @@
 namespace App\Console\Commands;
 
 use App\Channel;
-use App\Events\ChannelUpdated;
 use App\Jobs\SendFeedBySFTP;
 use App\Podcast\PodcastBuilder;
 use Illuminate\Console\Command;
 
-
-class BuildPodcast extends Command
+class BatchPodcasts extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'podcast:build {channelId}';
+    protected $signature = 'podcast:batch 
+        {--all : will generate all active podcasts } 
+        {--free : will generate only the free ones } 
+        {--early : will generate only the early birds } 
+        {--paying : will generate only paying channels }';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'This command will build one podcast feed at a time.';
+    protected $description = 'This command will podcast feeds by kind.';
 
     /**
      * Create a new command instance.
@@ -42,18 +44,23 @@ class BuildPodcast extends Command
      */
     public function handle()
     {
-        /**
-         * getting channel to build podcast for
-         */
+        
+        $channels = Channel::allActiveChannels()->filter(function ($channel){
+            var_dump(get_class($channel));
+        });
+        //var_dump(DB::connection()->getQueryLog());
+        /*
+        // getting channel to build podcast for
         $channel = Channel::findOrFail($this->argument('channelId'));
 
         if (PodcastBuilder::prepare($channel)->save()) {
-            /** uploading feed */
+            // uploading feed
             SendFeedBySFTP::dispatchNow($channel);
         }
         //event(new ChannelUpdated($channel));
-        
+
         $this->info("Podcast {{$channel->title()}} has been successfully created.");
-        $this->info("You can check it here : ".$channel->podcastUrl());
+        $this->info("You can check it here : " . $channel->podcastUrl());
+        */
     }
 }
