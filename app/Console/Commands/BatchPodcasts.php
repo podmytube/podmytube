aee5b418817b97caeed65b7790ce265c2daf5e40
frederick tyteca <frederick@tyteca.net>
2020-01-26 16:10:05 +0100

    batch podcast is now handling errors during process and displaying messages if wanted.

--- app/Console/Commands/BatchPodcasts.php
+++ app/Console/Commands/BatchPodcasts.php
@@ -5,11 +5,18 @@ namespace App\Console\Commands;
 use App\Channel;
 use App\Jobs\SendFeedBySFTP;
 use App\Podcast\PodcastBuilder;
+use App\Podcast\PodcastUrl;
+
 use Illuminate\Console\Command;
 use RuntimeException;
 
 class BatchPodcasts extends Command
 {
+    protected const _FAILURE = 0;
+    protected const _SUCCESS = 1;
+
+    protected $messages = [];
+
     /**
      * The name and signature of the console command.
      *
@@ -65,23 +72,78 @@ class BatchPodcasts extends Command
                 throw new RuntimeException("Option $optionTyped is not a valid one. Options available : free/paying/early/all.");
         }
 
-        $bar = $this->output->createProgressBar(count($channels));
-        $bar->start();
-
-        $finalMessage = PHP_EOL;
-        foreach ($channels as $channel) {
-            if (($podcastBuilderObj = PodcastBuilder::prepare($channel))->save()) {
-                // uploading feed
-                SendFeedBySFTP::dispatchNow($channel);
-                /* $this->info("Podcast {{$channel->title()}} has been successfully created.");
-                $this->info("You can check it here : " . $podcastBuilderObj->path()); */
-                $finalMessage .= "Channel {$channel->title()} {{$channel->channelId()}} has been generated " . PHP_EOL .
-                    " {{$podcastBuilderObj->path()}} - {{$channel->podcastUrl()}} " . PHP_EOL;
+        if (!$channels->count()) {
+            if ($this->getOutput()->isVerbose()) {
+                $this->info("There is no channels to generate for option {{$optionTyped}}");
             }
-            $bar->advance();
+            return true;
         }
 
-        $bar->finish();
-        $this->info($finalMessage);
+        if ($this->getOutput()->isVerbose()) {
+            $bar = $this->output->createProgressBar(count($channels));
+            $bar->start();
+        }
+
+        foreach ($channels as $channel) {
+            try {
+                if (PodcastBuilder::prepare($channel)->save()) {
+                    // uploading feed
+                    SendFeedBySFTP::dispatchNow($channel);
+                    $this->recordSuccess($channel);
+                }
+            } catch (\Exception $exception) {
+                $this->recordFailure($channel, $exception);
+            }
+            if ($this->getOutput()->isVerbose()) {
+                $bar->advance();
+            }
+        }
+
+        if ($this->getOutput()->isVerbose()) {
+            $bar->finish();
+        }
+
+        if ($this->getOutput()->isVeryVerbose()) {
+            $this->info(
+                PHP_EOL . implode(PHP_EOL, $this->getSuccess())
+            );
+        }
+
+        if ($this->getErrors()) {
+            $this->error($this->getErrors());
+        }
+    }
+
+    protected function recordSuccess(Channel $channel)
+    {
+        $this->addMessage(
+            self::_SUCCESS,
+            "Channel {$channel->title()} {{$channel->channelId()}} has been successfully generated {" .
+                PodcastUrl::prepare($channel)->get() . "}"
+        );
+    }
+
+    protected function recordFailure(Channel $channel, $exception)
+    {
+        $this->addMessage(
+            self::_FAILURE,
+            "Podcast generation has failed for Channel {$channel->title()} {{$channel->channelId()}} with "
+                . $exception->getMessage() . PHP_EOL
+        );
+    }
+
+    protected function addMessage($key, $value)
+    {
+        $this->messages[$key][] = $value;
+    }
+
+    protected function getErrors()
+    {
+        return $this->messages[self::_FAILURE] ?? null;
+    }
+
+    protected function getSuccess()
+    {
+        return $this->messages[self::_SUCCESS] ?? null;
     }
 }
