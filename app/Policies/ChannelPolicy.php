e3c00b75b5819e961eed4e63362e5bebf1becf03
Frederick Tyteca <frederick@tyteca.net>
2020-04-05 14:24:07 +0200

    wip

--- app/Policies/ChannelPolicy.php
+++ app/Policies/ChannelPolicy.php
@@ -5,13 +5,33 @@ namespace App\Policies;
 use App\Channel;
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Response;
 
 class ChannelPolicy
 {
-  use HandlesAuthorization;
+    use HandlesAuthorization;
+    public function __construct()
+    {
+        dump(__CLASS__ . "-" . __FUNCTION__);
+    }
 
-  public function owns(User $user, Channel $channel): bool
-  {
-    return $user->userId() == $channel->userId();
-  }
+    public function view(User $user, Channel $channel): bool
+    {
+        dd(__CLASS__ . '-' . __FILE__);
+        return $user->userId() == $channel->userId()
+            ? Response::allow()
+            : Response::deny('You do not own this post.');
+    }
+
+    public function index(User $user, Channel $channel): bool
+    {
+        dd(__CLASS__ . '-' . __FILE__);
+    }
+
+    public function update(User $user, Channel $channel): bool
+    {
+        return $user->userId() == $channel->userId()
+            ? Response::allow()
+            : Response::deny('You do not own this post.');
+    }
 }
