694a63708564675cdb1a718982d620f821f6db9c
frederick tyteca <frederick@tyteca.net>
2020-06-02 16:35:05 +0200

    simplifying apikeys management by removing environment

--- tests/Unit/ApiKeyModelTest.php
+++ tests/Unit/ApiKeyModelTest.php
@@ -12,29 +12,17 @@ class ApiKeyModelTest extends TestCase
 {
     use RefreshDatabase;
 
-    protected $developmentKeys;
-    protected $productionKeys;
+    protected $apikeys;
 
     public function setUp(): void
     {
         parent::setUp();
-
         // when only this test is run no problem.
         // however sometime RefreshDatabases does not clear table.
-        // so I'm forcing the truncate to me made
+        // so I'm forcing the truncate to be made
         DB::table('api_keys')->delete();
-
-        $this->developmentKeys = factory(ApiKey::class, 2)
-            ->create([
-                'environment' => ApiKey::LOCAL_ENV,
-            ])
-            ->map(function ($apikey) {
-                return $apikey->apikey;
-            });
-        $this->productionKeys = factory(ApiKey::class, 3)
-            ->create([
-                'environment' => ApiKey::PROD_ENV,
-            ])
+        $this->apikeys = factory(ApiKey::class, 2)
+            ->create()
             ->map(function ($apikey) {
                 return $apikey->apikey;
             });
@@ -42,15 +30,7 @@ class ApiKeyModelTest extends TestCase
 
     public function testGetOneLocalIsRunningFine()
     {
-        Config::set('app.env', 'local');
         $apikey = Apikey::make()->get();
-        $this->assertTrue($this->developmentKeys->contains($apikey));
-    }
-
-    public function testGetOneProductionIsRunningFine()
-    {
-        Config::set('app.env', 'production');
-        $apikey = Apikey::make()->get();
-        $this->assertTrue($this->productionKeys->contains($apikey));
+        $this->assertTrue($this->apikeys->contains($apikey));
     }
 }
