e0bd2206b0bbaa7c1232ce5f7a19a749772ae8cf
frederick tyteca <frederick@tyteca.net>
2020-06-23 10:56:54 +0200

    stopping to work on go project

---
+++ resources/views/emails/newsletters/2020-06-free-plan-update.blade.php
@@ -0,0 +1,32 @@
+@if (App::getLocale() == 'en')
+    I hope you are all doing great!
+
+This is my usual status update with all the things that I'm currently working on and excited about. 
+
+@else
+
+<p>
+    Voici ma premiere newsletter sur Podmytube !
+    Je me connais assez bien pour te dire que je ne sais pas quand sera la prochaine ni même s'il y en aura une.
+    Alors, sois tranquille, je ne vais pas te spammer.
+    Voici donc les sujets de cette newsletter.
+</p>
+
+<h2>Le gratuit reste gratuit mais ...</h2>
+<p>
+    Première chose, le forfait gratuit va changer.<br/> 
+    C'est malheureusement indispensable à cause de mes couts de stockage.<br/>
+    Depuis le début de l'aventure Podmytube, je conserve les épisodes générés quel que soit le forfait choisi.<br/>
+    <b>A partir de Juillet 2020, les épisodes ne seront plus conservés que 4 mois pour les podcasts gratuits</b>.<br/>
+</p>
+<p>
+    Si vous avez souscrit un forfait payant, aucun changement, les épisodes sont conservés aussi longtemps que vous êtes abonnés (voire davantage).
+    Si vous souhaitez que vos auditeurs puissent écouter tous vos épisodes, je vous invite donc à modifier votre forfait pour un forfait payant.
+</p>
+
+<h2>Modification des CGU</h2>
+<p>
+    Vous trouverez 
+</p>
+
+@endif
