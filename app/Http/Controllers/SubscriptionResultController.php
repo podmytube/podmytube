b34e2ba7d216138a21e1d7b3baf7ae96ecfeac5d
frederick tyteca <frederick@tyteca.net>
2020-07-07 10:46:16 +0200

    wip

--- app/Http/Controllers/SubscriptionResultController.php
+++ app/Http/Controllers/SubscriptionResultController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\StripeClient;
 
 class SubscriptionResultController extends Controller
 {
     public function success(Request $request)
     {
+        $stripe = new StripeClient(env("STRIPE_SECRET"));
+        $stripe->checkout->sessions->retrieve($request->get('session_id'), []);
+        dump($stripe);
         return view('subscription.success');
     }
 
---
+++ app/Jobs/StripeWebhooks/HandleCheckoutSessionCompleted.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs\StripeWebhooks;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Spatie\WebhookClient\Models\WebhookCall;
+
+class HandleCheckoutSessionCompleted implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /** @var \Spatie\WebhookClient\Models\WebhookCall */
+    public $webhookCall;
+
+    public function __construct(WebhookCall $webhookCall)
+    {
+        $this->webhookCall = $webhookCall;
+    }
+
+    public function handle()
+    {
+        // do your work here
+        info("checkout session completed");
+
+        // you can access the payload of the webhook call with `$this->webhookCall->payload`
+    }
+}
