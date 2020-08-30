7887dbdb3cf3682c47b8daba3a38b0d188f0c56a
frederick tyteca <frederick@tyteca.net>
2020-08-30 11:56:13 +0200

    plausible another try

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,7 +4,3 @@
 	<a href="{{route('terms')}}">Terms</a> - <a href="{{ route('privacy') }}">Privacy</a>
 	&copy; 2017-2020 <a href="https://www.podmytube.com">Podmytube</a>
 </p>
-
-@if (App::environment('production'))
-<script async defer data-domain="podmytube.com" src="https://plausible.io/js/plausible.js"></script>
-@endif
