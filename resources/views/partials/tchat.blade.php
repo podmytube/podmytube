6c8211a37c60d18c14bd7651a8c5cb89e188e32a
frederick tyteca <frederick@tyteca.net>
2020-08-30 12:18:53 +0200

    button is customized and positionned

--- resources/views/partials/tchat.blade.php
+++ resources/views/partials/tchat.blade.php
@@ -1,9 +1,14 @@
-<div id="floating-chat" class="text-right"></div>
+<div id="floating-chat"></div>
 
 <script type="text/javascript">
   $(function () {
     $('#floating-chat').floatingWhatsApp({
-      phone: '33615837006'
+      phone: '33615837006',
+      popupMessage: 'Hello, how can I help you ?',
+      showPopup: true,
+      headerTitle: 'Welcome !',
+      headerColor: '#ce1701',
+      position: 'right',
     });
   });
 </script>
