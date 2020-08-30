5a032312bf9031e9cf56af70f0cf7ea4c97c706f
frederick tyteca <frederick@tyteca.net>
2020-08-30 11:50:12 +0200

    removing google tag manager and adding plausible.io

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,3 +4,7 @@
 	<a href="{{route('terms')}}">Terms</a> - <a href="{{ route('privacy') }}">Privacy</a>
 	&copy; 2017-2020 <a href="https://www.podmytube.com">Podmytube</a>
 </p>
+
+@if (App::environment('production'))
+<script async defer data-domain="podmytube.com" src="https://plausible.io/js/plausible.js"></script>
+@endif
