6342cc28577038e0e375dcdc51fae91a7676b27f
frederick tyteca <frederick@tyteca.net>
2020-09-21 18:48:09 +0200

    first post has been written. Post showing is up. Index must be done

--- resources/views/partials/share.blade.php
+++ resources/views/partials/share.blade.php
@@ -1,13 +1,35 @@
-<ul>
-    <li>
-        <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}&t={{ urlencode($title) }}">
-            facebook
-        </a>
-    </li>
-    <li>
-        <a href="https://twitter.com/intent/tweet?text=Introducing%20Castos&url={{ urlencode($url) }}"> twitter </a>
-    </li>
-    <li>
-        <a href="https://www.linkedin.com/shareArticle?url={{ urlencode($url) }}&title={{ urlencode($url) }}&mini=true"> linkedIn </a>
-    </li>
-</ul>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item">
+                        <a href="https://twitter.com/intent/tweet?text={{ urlencode($title) }}&url={{ $url }}">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}&t={{ urlencode($title) }}">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://www.linkedin.com/shareArticle?url={{ urlencode($url) }}&title={{ urlencode($title) }}&mini=true">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-linkedin fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+                <p class="copyright text-muted">Copyright &copy; Podmytube 2020</p>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.blog')
 
 @section('pageTitle', __('medias.page_title_index') )
 
