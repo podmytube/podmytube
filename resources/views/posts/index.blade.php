ea0951d369069c012edc4b1ca359f880260e6015
frederick tyteca <frederick@tyteca.net>
2020-09-22 21:33:34 +0200

    blog is seeable and let s start with that

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,19 +4,38 @@
 
 @section ('content')
 
+<!-- Page Header -->
+<header class="masthead">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="site-heading">
+                    <h1>News from Podmytube</h1>
+                    <span class="subheading">Let's talk together.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
 @if ($posts->count())
 @foreach($posts as $post)
-<div>
-    <div>
-        <img src="{{ $post->featured_image }}">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-preview">
+                <a href="{{ route('post.show', $post) }}">
+                    <h2 class="post-title">{!! $post->title() !!}</h2>
+                    <p class="post-subtitle">{!! $post->excerpt !!}</p>
+                </a>
+                <p class="post-meta">Posted by {{ $post->author }} on {{ $post->lastUpdate() }}</p>
+            </div>
+            <hr>
+        </div>
     </div>
-    <a href="{{ route('post.show', $post) }}">{{ $post->title() }}</a>
-    {!! $post->excerpt !!}
 </div>
 @endforeach
-@else
-<div class="alert alert-dark" role="alert">
-    {!! __('medias.index_channel_has_no_known_episode') !!}
-</div>
 @endif
+
 @endsection
