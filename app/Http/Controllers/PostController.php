2bd8d6ed3b6b046144e48ea210827d4ebc1554a9
frederick tyteca <frederick@tyteca.net>
2020-10-03 10:06:34 +0200

    fixing blog post listing

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('published_at', 'desc')->get();
         return view('posts.index', compact('posts'));
     }
 
