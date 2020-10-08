d8daaa3a3efcc5d203530a2ef5c7d0adf322641c
frederick tyteca <frederick@tyteca.net>
2020-10-08 22:18:26 +0200

    trying to work with one template. finding it ugly.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,30 @@
 
 @section('content')
 
+<!--Hero-->
+<div class="pt-24">
+
+	<div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+		<!--Left Col-->
+		<div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+			<p class="uppercase tracking-loose w-full">Host your podcast right from your youtube channel</p>
+			<h1 class="my-4 text-5xl font-bold leading-tight">From youtube to iTunes in seconds</h1>
+			<p class="leading-normal text-2xl mb-8">Sub-hero message, not too long and not too short. Make it just right!</p>
+		
+			
+
+			<button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Subscribe</button>
+			
+		</div>
+		<!--Right Col-->
+		<div class="w-full md:w-3/5 py-6 text-center">
+			<img class="w-full md:w-4/5 z-50" src="hero.png">
+		</div>		
+	</div>
+
+</div>
+
+
 <main role="main" class="container" id="main">
 
     @include ('partials.navbar')
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,18 +1,11 @@
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('pageTitle') - {{ config('app.name', 'Dashboard') }} - PodMyTube</title>
-
-    @yield('recaptcha')
-
-    @yield('stripeJs')
-
-    <script src="{{ asset('js/app.js') }}"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('pageTitle') - PodmyTube</title>
+    <meta name="description" content="Hosting youtube podcasts">
+    <meta name="keywords" content="youtube, podcast, podcats, hosting, converting">
+    <meta name="author" content="Frederick Tyteca">
 
     <!--Favicon-->
     <link rel="icon" type="image/png" href="/favicon.png" />
@@ -22,8 +15,29 @@
 
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 
+    @yield('recaptcha')
+
+    @yield('stripeJs')
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     @if (App::environment('production'))
     <script async defer data-domain="podmytube.com" src="https://stats.podmytube.com/js/index.js"></script>
     @endif
 
+  <!-- Font Awesome if you need it
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+  -->
+  
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
+
+  <!-- Define your gradient here - use online tools to find a gradient matching your branding-->
+	<style>
+		.gradient {
+			background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
+		}
+	</style>
 </head>
