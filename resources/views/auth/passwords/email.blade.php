f21df5960eb28719c9e94ecb40a42960f250874e
frederick tyteca <frederick@tyteca.net>
2020-10-09 20:38:44 +0200

    filling email form with previous email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -17,7 +17,7 @@
 	@endif
 
 	<div class="form-label-group">
-		<input type="email" name="email" id="inputEmail" class="form-control" placeholder="{{ __('messages.email_label') }}" value="{{ old('email') }}" required autofocus />
+		<input type="email" name="email" id="inputEmail" class="form-control" placeholder="{{ __('messages.email_label') }}" value="{{ $email }}" required autofocus />
 		<label for="inputEmail">{{ __('messages.email_label') }}</label>
 	</div>
 
