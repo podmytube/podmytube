134508de9bd1211e3103d488b3d53a8686a99a70
frederick tyteca <frederick@tyteca.net>
2020-10-11 19:44:02 +0200

    removed jquery, trying now to rework my existing design with tailwind

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -5,11 +5,11 @@
     <ol class="breadcrumb bg-white">
         @foreach ($breadcrumbs as $breadcrumb)
 
-            @if ($breadcrumb->url && !$loop->last)
-                <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
-            @else
-                <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
-            @endif
+        @if ($breadcrumb->url && !$loop->last)
+        <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+        @else
+        <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
+        @endif
 
         @endforeach
     </ol>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,43 +1,34 @@
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('pageTitle') - PodmyTube</title>
-    <meta name="description" content="Hosting youtube podcasts">
-    <meta name="keywords" content="youtube, podcast, podcats, hosting, converting">
-    <meta name="author" content="Frederick Tyteca">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('pageTitle') - PodmyTube</title>
+  <meta name="description" content="Hosting youtube podcasts">
+  <meta name="keywords" content="youtube, podcast, podcats, hosting, converting">
+  <meta name="author" content="Frederick Tyteca">
 
-    <!--Favicon-->
-    <link rel="icon" type="image/png" href="/favicon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <!--Favicon-->
+  <link rel="icon" type="image/png" href="/favicon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 
-    @yield('recaptcha')
+  @yield('recaptcha')
 
-    @yield('stripeJs')
+  @yield('stripeJs')
 
-    <script src="{{ asset('js/app.js') }}"></script>
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    @if (App::environment('production'))
-    <script async defer data-domain="podmytube.com" src="https://stats.podmytube.com/js/index.js"></script>
-    @endif
+  @if (App::environment('production'))
+  <script async defer data-domain="podmytube.com" src="https://stats.podmytube.com/js/index.js"></script>
+  @endif
 
   <!-- Font Awesome if you need it
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
   -->
-  
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
 
-  <!-- Define your gradient here - use online tools to find a gradient matching your branding-->
-	<style>
-		.gradient {
-			background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
-		}
-	</style>
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
 </head>
