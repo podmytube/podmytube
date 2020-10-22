8c8d018b12f0cd85ed79eda727a3af488a3e3a03
frederick tyteca <frederick@tyteca.net>
2020-10-22 22:51:57 +0200

    wip

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <header class="container mx-auto sticky top-0 z-20 bg-gray-900 pt-2 pl-2 pr-4" style="opacity:0.95;">
     <nav id="nav" class="flex items-center justify-between flex-wrap sm:pt-6 sm:pb-6 md:pt-6 md:pb-6 lg:pt-10 lg:pb-10">
-        <div class="flex items-center flex-shrink-0 text-white mr-8">
+        <div class=" flex-shrink-0 text-white mr-8">
             <a href="{{ route('www.index') }}"><img src="/images/podmytube-logo-2020-150x53.png"></a>
         </div>
         <div class="block md:hidden">
@@ -12,7 +12,7 @@
                 </svg>
             </button>
         </div>
-        <div id="mobile-nav" class="hidden text-center w-full flex-grow md:text-left md:pt-1 md:flex md:items-center md:w-auto">
+        <div id="mobile-nav" class="hidden text-center text-white w-full flex-grow md:text-left md:pt-1 md:flex md:items-center md:w-auto">
             <div class="text-sm md:flex-grow">
                 <a href="{{ route('pricing') }}"
                     class="block mt-4 md:inline-block md:mt-0 text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2 mr-0 md:mr-4">
@@ -28,46 +28,43 @@
                 </a>
             </div>
             @guest
-            <div id="login-signup">
+            <div class="text-sm">
                 <a href="{{ route('login') }}"
                     class="block mt-4 md:inline-block md:mt-0 text-sm text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2 mr-0 md:mr-4">
                     Log In
                 </a>
                 <a href="{{ route('register') }}"
-                    class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 md:mt-0">Sign
-                    Up</a>
-            </div>
-            @else
-            <div id="signed-in">
-                <a href="{{ route('channel.create') }}"
-                    class="block mt-4 border border-white rounded-lg md:inline-block md:mt-0 text-sm font-semibold text-white p-2 hover:bg-white hover:text-gray-900 md:mr-4">
-                    Add your podcast
-                </a>
-                <a href="{{ route('logout') }}" 
-                class="block mt-4 md:inline-block md:mt-0 text-sm font-semibold text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2 md:mr-4"
-                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    Logout</a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                <a href="{{ route('home') }}"
-                    class="block mt-4 md:inline-block md:mt-0 text-sm font-semibold text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2">
-                    Your Dashboard →
+                    class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 md:mt-0">
+                    Sign Up
                 </a>
             </div>
             @endguest
+            <div class="text-sm">
+                <a href="{{ route('channel.create') }}"
+                    class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 md:mt-0">
+                    Add your podcast
+                </a>
+                <a href="{{ route('logout') }}" 
+                    class="block mt-4 md:inline-block md:mt-0 text-sm font-semibold text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2 md:mr-4"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </div>
         </div>
     </nav>
-    <script>
-        document.addEventListener("DOMContentLoaded", function(event) {
-            document.getElementById("mobile-nav-trigger").addEventListener("click", function(event){
-                var menuLinks = document.getElementById('mobile-nav');
-                if (menuLinks.style.display == "" || menuLinks.style.display == "none") {
-                    menuLinks.style.display = "block";
-                } else {
-                    menuLinks.style.display = "none";
-                }
-            });
-        });
-    </script>
 </header>
+<script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        document.getElementById("mobile-nav-trigger").addEventListener("click", function(event){
+            var menuLinks = document.getElementById('mobile-nav');
+            if (menuLinks.style.display == "" || menuLinks.style.display == "none") {
+                menuLinks.style.display = "block";
+            } else {
+                menuLinks.style.display = "none";
+            }
+        });
+    });
+</script>
