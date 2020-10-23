6ebd58ad523b49d3e40a03a3d652c4a4aec9fccb
frederick tyteca <frederick@tyteca.net>
2020-10-23 21:20:32 +0200

    js dependenciespackage.json public/mix-manifest.json resources/js/app.js webpack.mix.js

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,70 +1,78 @@
-<header class="container mx-auto sticky top-0 z-20 bg-gray-900 pt-2 pl-2 pr-4" style="opacity:0.95;">
-    <nav id="nav" class="flex items-center justify-between flex-wrap sm:pt-6 sm:pb-6 md:pt-6 md:pb-6 lg:pt-10 lg:pb-10">
-        <div class=" flex-shrink-0 text-white mr-8">
-            <a href="{{ route('www.index') }}"><img src="/images/podmytube-logo-2020-150x53.png"></a>
+<header class="text-gray-100 sm:flex sm:justify-between sm:py-2 sm:px-4">
+    <div class="flex items-center justify-between py-2 sm:p-0">
+        <div>
+            <a href="{{ route('www.index') }}"><img class="h-12" src="/images/podmytube-logo-2020-150x53.png"></a>
         </div>
-        <div class="block md:hidden">
-            <button id="mobile-nav-trigger"
-                class="flex items-center px-3 py-2 border rounded text-white border-black-400 hover:text-white hover:border-white">
-                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <div class="pr-2 sm:hidden">
+            <button id="mobile-nav-trigger" class="block text-gray-400 hover:text-white focus:text-white focus:outline-none">
+                <svg class="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <title>Menu</title>
                     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
                 </svg>
             </button>
         </div>
-        <div id="mobile-nav" class="hidden text-center text-white w-full flex-grow md:text-left md:pt-1 md:flex md:items-center md:w-auto">
-            <div class="text-sm md:flex-grow">
-                <a href="{{ route('pricing') }}"
-                    class="block mt-4 md:inline-block md:mt-0 text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2 mr-0 md:mr-4">
-                    Pricing
-                </a>
-                <a href="{{ route('post.index') }}"
-                    class="block mt-4 md:inline-block md:mt-0 text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2 mr-0 md:mr-4">
-                    Blog
-                </a>
-                <a href="{{ route('about') }}"
-                    class="block mt-4 md:inline-block md:mt-0 text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2 mr-0 md:mr-4">
-                    About
-                </a>
-            </div>
-            @guest
-            <div class="text-sm">
-                <a href="{{ route('login') }}"
-                    class="block mt-4 md:inline-block md:mt-0 text-sm text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2 mr-0 md:mr-4">
-                    Log In
-                </a>
-                <a href="{{ route('register') }}"
-                    class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 md:mt-0">
-                    Sign Up
-                </a>
-            </div>
-            @endguest
-            <div class="text-sm">
-                <a href="{{ route('channel.create') }}"
-                    class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 md:mt-0">
-                    Add your podcast
-                </a>
-                <a href="{{ route('logout') }}" 
-                    class="block mt-4 md:inline-block md:mt-0 text-sm font-semibold text-white md:border-b-2 md:border-transparent md:hover:border-white md:hover:border-b-2 md:mr-4"
-                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    Logout
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </div>
-        </div>
-    </nav>
+    </div>
+    <div class="hidden pt-2 pb-4 text-center sm:flex sm:items-center sm:p-0" id="mobile-nav">
+        <a class="     block px-2 py-1 text-gray-100 rounded hover:bg-gray-800" href="{{ route('pricing') }}"> Pricing </a>
+        <a class="mt-1 block px-2 py-1 text-gray-100 rounded hover:bg-gray-800 sm:mt-0 sm:mr-2" href="{{ route('post.index') }}"> Blog </a>
+        <a class="mt-1 block px-2 py-1 text-gray-100 rounded hover:bg-gray-800 sm:mt-0 sm:mr-2" href="{{ route('about') }}"> About </a>
+        @guest
+        <a class="mt-1 block px-2 py-1 text-gray-100 rounded hover:bg-gray-800 sm:mt-0 sm:mr-2" href="{{ route('login') }}"> Log In </a>
+        <a class="mt-1 block px-2 py-1 text-gray-100 rounded hover:bg-gray-800 sm:mt-0 sm:mr-2 sm:hover:bg-white sm:border sm:rounded sm:border-white hover:border-transparent hover:text-gray-900" href="{{ route('register') }}"> Sign Up </a>
+        @else
+        <a class="mt-1 block px-2 py-1 text-gray-100 rounded hover:bg-gray-800 sm:mt-0 sm:mr-2" href="{{ route('channel.create') }}"> Add you podcast </a>
+        <a class="mt-1 block px-2 py-1 text-gray-100 rounded hover:bg-gray-800 sm:mt-0 sm:mr-2" href="#" id="logout-link"> Logout </a>
+        @endguest
+    </div>
 </header>
+
+<form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+    @csrf
+</form>
+
 <script>
     document.addEventListener("DOMContentLoaded", function(event) {
+        var navIsOpen=false;
+
+        var logoutLink=document.getElementById("logout-link");
+        if(logoutLink){
+            logoutLink.addEventListener("click", function(event){
+                document.getElementById('logout-form').submit();
+            });
+        }
+        
         document.getElementById("mobile-nav-trigger").addEventListener("click", function(event){
-            var menuLinks = document.getElementById('mobile-nav');
-            if (menuLinks.style.display == "" || menuLinks.style.display == "none") {
-                menuLinks.style.display = "block";
-            } else {
-                menuLinks.style.display = "none";
-            }
+            toggleNav(document.getElementById("mobile-nav"));
         });
+
+        function toggleNav(element){
+            if (navIsOpen){
+                // hide it
+                removeClass(element, "block");
+                addClass(element, "hidden");
+                navIsOpen=false;
+                return true;
+            }
+            // show it
+            removeClass(element, "hidden");
+            addClass(element, "block");
+            navIsOpen=true;
+            return true;
+        }
+
+        function hasClass(element, className) {
+            return !!element.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+        }
+
+        function addClass(element, className) {
+            if (!hasClass(element, className)) element.className += " " + className;
+        }
+
+        function removeClass(element, className) {
+            if (hasClass(element, className)) {
+                var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+                element.className = element.className.replace(reg, ' ');
+            }
+        }
     });
 </script>
