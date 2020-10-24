a6ae718dead811c1fee83e1cb1c4397437e78f28
frederick tyteca <frederick@tyteca.net>
2020-10-25 00:25:32 +0200

    my shrinktest

---
+++ resources/views/partials/shrinkTest.blade.php
@@ -0,0 +1,8 @@
+<div class="container p-10 mx-auto flex text-white">
+    <div class="flex-shrink-0 w-1/2 bg-gray-800">
+        <p>lorem ipsum</p>
+    </div>
+    <div class="flex-shrink-0 flex-grow-0 w-1/2  bg-purple-800">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>
