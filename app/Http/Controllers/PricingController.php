5176d2cf5c94a2f730a316aedea0ec610bc12ed2
frederick tyteca <frederick@tyteca.net>
2020-12-30 21:30:10 +0100

    wip

--- app/Http/Controllers/PricingController.php
+++ app/Http/Controllers/PricingController.php
@@ -2,12 +2,52 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PricingController extends Controller
 {
     public function index()
     {
-        return view('pricing');
+        $plans = [
+            'Business' => [
+                'title' => 'Business',
+                'monthly_price' => 79,
+                'features' => [
+                    '30 episodes / month',
+                    'Add exclusive content',
+                    'Live customer support',
+                    'Episodes are kept unlimited time',
+                ],
+            ],
+            'Professionnal' => [
+                'title' => 'Professionnal',
+                'monthly_price' => 29,
+                'features' => [
+                    '12 episodes / month',
+                    'All episodes kept',
+                    'Add exclusive content',
+                    'Live customer support',
+                    'Episodes are kept unlimited time',
+                ],
+            ],
+            'Starter' => [
+                'title' => 'Starter',
+                'monthly_price' => 9,
+                'features' => [
+                    '5 videos / month',
+                    'All episodes kept',
+                    'Add exclusive content',
+                    'Live customer support',
+                    'Episodes are kept unlimited time',
+                ],
+            ],
+            'Free' => [
+                'title' => 'Free',
+                'monthly_price' => 0,
+                'features' => [
+                    '1 video / month',
+                    'Episodes are kept 3 monthes',
+                ],
+            ]
+        ];
+        return view('pricing', compact('plans'));
     }
 }
