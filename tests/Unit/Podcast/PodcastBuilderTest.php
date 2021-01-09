282b46044b36db211cfbadcf56ac7ff919553470
frederick tyteca <frederick@tyteca.net>
2021-01-09 17:39:56 +0100

    WIP les podcasts sont generes, me reste a les sauvegarder-uploader

--- tests/Unit/Podcast/PodcastBuilderTest.php
+++ tests/Unit/Podcast/PodcastBuilderTest.php
@@ -10,34 +10,65 @@ use Illuminate\Foundation\Testing\WithFaker;
 
 class PodcastBuilderTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
+    use RefreshDatabase,
+        WithFaker;
 
+    /** @var \App\Channel $channel */
     protected $channel;
+
+    /** @var \App\Media $medias */
     protected $medias;
+
+    /** @var string $renderedPodcast */
+    protected $renderedPodcast;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->channel = $this->createChannelWithPlan();
-        $this->addMediasToChannel($this->channel, 5, true);
         factory(Thumb::class)->create(['channel_id' => $this->channel->channel_id, ]);
     }
 
-    public function testRenderingWholePodcast()
+    public function testRenderingPodcastWithoutItemsShouldBeGood()
     {
-        $attributes = [
-        ];
-        $renderedPodcast = PodcastBuilder::create($attributes)->render();
+        $this->renderedPodcast = PodcastBuilder::create($this->channel->toPodcast())->render();
+        $this->headerChecking();
+    }
 
-        $this->assertStringContainsString('<link>' . $this->channel->link . '</link>', $renderedPodcast);
-        $this->assertStringContainsString('<title>' . $this->channel->title() . '</title>', $renderedPodcast);
-        $this->assertStringContainsString('<description><![CDATA[' . $this->channel->description . ']]></description>', $renderedPodcast);
-        $this->assertStringContainsString('<copyright>' . $this->channel->podcast_copyright . '</copyright>', $renderedPodcast);
-        $this->assertStringContainsString('<language>' . $this->channel->language->code . '</language>', $renderedPodcast);
-        $this->assertStringContainsString('<image>', $renderedPodcast);
-        $this->assertStringContainsString('<url>' . $this->channel->thumb->podcastUrl() . '</url>', $renderedPodcast);
-        $this->assertStringContainsString('<title>' . $this->channel->title() . '</title>', $renderedPodcast);
-        $this->assertStringContainsString('<link>' . $this->channel->link . '</link>', $renderedPodcast);
-        $this->assertStringContainsString('</image>', $renderedPodcast);
+    public function testRenderingPodcastWithItemsShouldBeGoodToo()
+    {
+        $this->addMediasToChannel($this->channel, 5, true);
+        $this->renderedPodcast = PodcastBuilder::create($this->channel->toPodcast())->render();
+        $this->headerChecking();
+        $this->itemsChecking();
+    }
+
+    public function itemsChecking()
+    {
+        $this->channel->mediasToPublish()->map(function ($media) {
+            $this->assertStringContainsString('<guid>' . $media->media_id . '</guid>', $this->renderedPodcast);
+            $this->assertStringContainsString('<title>' . $media->title . '</title>', $this->renderedPodcast);
+            $this->assertStringContainsString(
+                '<enclosure url="' . $media->enclosureUrl() . '" length="' . $media->length . '" type="audio/mpeg" />',
+                $this->renderedPodcast
+            );
+            $this->assertStringContainsString('<pubDate>' . $media->pubDate() . '</pubDate>', $this->renderedPodcast);
+            $this->assertStringContainsString('<itunes:duration>' . $media->duration() . '</itunes:duration>', $this->renderedPodcast);
+            $this->assertStringContainsString('<itunes:explicit>' . $media->explicit === true ? 'true' : 'false' . '</itunes:explicit>', $this->renderedPodcast);
+        });
+    }
+
+    public function headerChecking()
+    {
+        $this->assertStringContainsString('<link>' . $this->channel->link . '</link>', $this->renderedPodcast);
+        $this->assertStringContainsString('<title>' . $this->channel->title() . '</title>', $this->renderedPodcast);
+        $this->assertStringContainsString('<description><![CDATA[' . $this->channel->description . ']]></description>', $this->renderedPodcast);
+        $this->assertStringContainsString('<copyright>' . $this->channel->podcast_copyright . '</copyright>', $this->renderedPodcast);
+        $this->assertStringContainsString('<language>' . $this->channel->language->code . '</language>', $this->renderedPodcast);
+        $this->assertStringContainsString('<image>', $this->renderedPodcast);
+        $this->assertStringContainsString('<url>' . $this->channel->thumb->podcastUrl() . '</url>', $this->renderedPodcast);
+        $this->assertStringContainsString('<title>' . $this->channel->title() . '</title>', $this->renderedPodcast);
+        $this->assertStringContainsString('<link>' . $this->channel->link . '</link>', $this->renderedPodcast);
+        $this->assertStringContainsString('</image>', $this->renderedPodcast);
     }
 }
