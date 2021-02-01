d9381153520f6a1223840246380dda173455353d
frederick tyteca <frederick@tyteca.net>
2021-02-01 20:54:07 +0100

    scheduling playlist update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,9 +4,12 @@ namespace App\Console;
 
 use App\Console\Commands\CleanFreeChannelMedias;
 use App\Console\Commands\DownloadVideosByPeriodCommand;
+use App\Console\Commands\GetPlaylistsCommand;
 use App\Console\Commands\LastMediaPublishedChecker;
+use App\Console\Commands\SendMonthlyReports;
 use App\Console\Commands\UpdateBlogPostsCommand;
 use App\Console\Commands\UpdateChannelsCommand;
+use App\Console\Commands\UpdatePlaylistsForPayingChannelsCommand;
 use App\Console\Commands\UpdatePodcastsCommand;
 use App\Console\Commands\UpdateSitemapCommand;
 use Illuminate\Console\Scheduling\Schedule;
@@ -33,28 +36,34 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         /** generating sitemap */
-        $schedule->command(UpdateSitemapCommand::class)->hourly();
+        $schedule->command(UpdateSitemapCommand::class)->daily();
 
         /** grabbing videos */
         $schedule->command(DownloadVideosByPeriodCommand::class)->hourlyAt(32);
 
-        /** cleaning free medias old episodes */
+        /** cleaning free medias old episodes - 12h */
         $schedule->command(CleanFreeChannelMedias::class)->monthlyOn($day = 1, $time = '12:0');
 
-        /** updating channels */
+        /** updating channels - 2h */
         $schedule->command(UpdateChannelsCommand::class, ['all'])->hourlyAt(2);
 
         /** Check media */
         $schedule->command(LastMediaPublishedChecker::class)->everySixHours();
 
         /** Check blog post */
-        $schedule->command(UpdateBlogPostsCommand::class)->everyFifteenMinutes();
+        $schedule->command(UpdateBlogPostsCommand::class)->dailyAt('23h27');
 
         /** Building podcasts */
         $schedule->command(UpdatePodcastsCommand::class, ['all'])->hourlyAt(50);
 
         /** monthly report on first monday */
         $schedule->command(SendMonthlyReports::class)->monthly()->days([1])->at('11:00');
+
+        /** get active playlists */
+        $schedule->command(GetPlaylistsCommand::class)->hourlyAt(50);
+
+        /** generate playlists */
+        $schedule->command(UpdatePlaylistsForPayingChannelsCommand::class)->hourlyAt(45);
     }
 
     /**
