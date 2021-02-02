cc807a9e875bbe8734a5d20c4aa2c3d16d9836fe
frederick tyteca <frederick@tyteca.net>
2021-02-02 07:14:38 +0100

    script to be run for every prod update

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+containerName=dashboard.podmytube.com
+
+# updating code
+git pull
+
+# creating symlink from public to storage
+if [[ ! -L "./public/storage" ]]; then
+	docker exec -it --user www-data $containerName php artisan storage:link
+fi
+
+if [[ ! -L "./public/medias" ]]; then
+	ln -s public/storage public/medias
+fi
+
+# installing php modules
+composer install --ignore-platform-reqs
+
+# building css/js
+npm install && npm run production
+
+# updating database migrations
+docker exec -it --user www-data $containerName php artisan migrate
+
+# emptying failed_jobs table
+docker exec -it --user www-data $containerName php artisan queue:flush
+
+# restart queue to avoid cache problems
+docker exec -it --user www-data $containerName php artisan queue:restart
+
+# clearing all cache
+docker exec -it --user www-data $containerName php artisan optimize:clear
