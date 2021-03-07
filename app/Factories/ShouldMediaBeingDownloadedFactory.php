a71e17c3938107e993ecd9bc42643df25e0e8400
frederick tyteca <frederick@tyteca.net>
2021-03-07 12:17:08 +0100

    need to check if media has been published recently and replace part of lastMediaChecker by ShouldMediaBeingDownloaded

--- app/Factories/ShouldMediaBeingDownloadedFactory.php
+++ app/Factories/ShouldMediaBeingDownloadedFactory.php
@@ -3,6 +3,7 @@
 namespace App\Factories;
 
 use App\Exceptions\DownloadMediaTagException;
+use App\Exceptions\MediaAlreadyGrabbedException;
 use App\Exceptions\MediaIsTooOldException;
 use App\Exceptions\YoutubeMediaIsNotAvailableException;
 use App\Media;
@@ -34,25 +35,32 @@ class ShouldMediaBeingDownloadedFactory
      * check if media is eligible for download.
      * check filters and date.
      *
+     * @throws MediaAlreadyGrabbedException
      * @throws YoutubeMediaIsNotAvailableException
      * @throws MediaIsTooOldException
      * @throws DownloadMediaTagException
      */
     public function check():bool
     {
-        Log::notice("Getting informations for media {$this->media->media_id}");
+        /** if already grabbed return false */
+        if ($this->isMediaAlreadyGrabbed()) {
+            $message = "Media {$this->media->media_id} already grabbed for {$this->media->channel->nameWithId()}. No alert to send.";
+            Log::notice($message);
+            throw new MediaAlreadyGrabbedException($message);
+        }
+
         $youtubeVideo = YoutubeVideo::forMedia($this->media->media_id);
 
         /** is video downladable (not upcoming and processed) */
         if (!$youtubeVideo->isAvailable()) {
-            $message = "This video {$this->media->media_id} is not available yet. 'upcoming' live or not yet 'processed'.";
+            $message = "The video {$this->media->media_id} for {$this->media->channel->nameWithId()} is not available yet. 'upcoming' live or not yet 'processed'.";
             Log::notice($message);
             throw new YoutubeMediaIsNotAvailableException($message);
         }
 
         /** check if media is not too old */
         if (!$this->media->channel->isDateAccepted($this->media->published_at)) {
-            $message = "This video {$this->media->media_id} is too old to be downloaded.";
+            $message = "The video {$this->media->media_id} for {$this->media->channel->nameWithId()} is too old to be downloaded.";
             Log::notice($message);
             throw new MediaIsTooOldException($message);
         }
@@ -60,11 +68,16 @@ class ShouldMediaBeingDownloadedFactory
         /** if media has a tag, is it downladable */
         if (!$this->media->channel->areTagsAccepted($youtubeVideo->tags())) {
             $message = 'Media tags ' . implode(',', $youtubeVideo->tags()) .
-                    " are not in allowed tags {$this->media->channel->accept_video_by_tag}.";
+                    " are not in allowed tags {$this->media->channel->accept_video_by_tag} for {$this->media->channel->nameWithId()}.";
             Log::notice($message);
             throw new DownloadMediaTagException($message);
         }
 
         return true;
+    }
+
+    public function isMediaAlreadyGrabbed():bool
+    {
+        return $this->media->hasBeenGrabbed();
     }
 }
