82bedbc49137c6bd1f460ee069d0e9a4aa4e9811
frederick tyteca <frederick@tyteca.net>
2021-03-21 18:54:41 +0100

    wip

---
+++ resources/views/emails/css.blade.php
@@ -0,0 +1,112 @@
+<style>
+        body {
+            margin: 0 20px;
+            font-family: Heebo, Helvetica, Arial, sans-serif;
+            color: #111827; /** gray-900 */
+        }
+        .text-gray-900{ 
+            color:#111827; 
+        }
+        .underline{
+            text-decoration: underline;
+        }
+        h1 {
+            color: #555555;
+        }
+        a, a:hover, a:visited{
+            text-decoration:none;
+        }
+        .text-is-grey {
+            color: grey;
+        }
+        .text-success {
+            color: #28a745;
+        }
+        .text-danger {
+            color: #dc3545;
+        }
+        #logo img {
+            margin-left: auto;
+            margin-right: auto;
+            display: block;
+        }
+        .bgsuccess {
+            background-color: #28a745;
+        }
+
+        .bgfailure {
+            background-color: #dc3545;
+        }
+
+        .button {
+            border: none;
+            color: white;
+            padding: 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            border-radius: 12px;
+        }
+
+        .bordered {
+            border: 1px solid grey;
+        }
+        a,a:hover{
+            text-decoration: none;
+        }
+        .h-4 {
+            height: 1.25rem;
+        }
+        .h-6 {
+            height: 1.5rem;
+        }
+        .vamiddle{
+            vertical-align:middle;
+        }
+        .fill-current {
+            fill: currentColor;
+        }
+        .w-auto {
+            width: auto;
+        }
+        .bold{
+            font-weight: 600;
+        }
+        .button{
+            background-color: #059669;
+            color: #F9FAFB;
+            box-shadow: 0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06) !important;
+            text-transform: uppercase;
+            padding-top: .625rem;
+            padding-bottom: .625rem;
+            padding-left: 1.25rem;
+            padding-right: 1.25rem;
+            font-size: .875rem;
+            line-height: 1.25rem;
+            letter-spacing: 0.10rem;
+            border-radius: .375rem;
+            /* display: flex;
+            align-items: center; */
+        }
+        table,tr,td{
+            --border-opacity: 1;
+            border-color: rgba(255, 255, 255, var(--border-opacity));
+        }
+        table {
+        width: 100%;
+        }
+        td{
+            padding: 0.5rem;
+            --text-opacity: 1;
+            color: rgba(26, 32, 44, var(--text-opacity));
+        }
+        th {
+            padding: 0.75rem;
+            --bg-opacity: 1;
+            background-color: rgba(26, 32, 44, var(--bg-opacity));
+            color: white;
+        }
+        tr:nth-child(even){background-color: #f2f2f2;}
+    </style>
