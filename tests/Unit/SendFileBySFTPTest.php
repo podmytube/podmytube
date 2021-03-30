bf2dfc73fcf828f26e2b6158705e31136188d4c4
frederick tyteca <frederick@tyteca.net>
2021-03-30 22:43:28 +0200

    brand new class niceSSH

--- tests/Unit/SendFileBySFTPTest.php
+++ tests/Unit/SendFileBySFTPTest.php
@@ -6,6 +6,7 @@ use App\Jobs\SendFileBySFTP;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Storage;
 use phpseclib\Crypt\RSA;
+use phpseclib\Net\SFTP;
 use phpseclib\Net\SSH2;
 use Tests\TestCase;
 
@@ -44,17 +45,38 @@ class SendFileBySFTPTest extends TestCase
         $host = config('filesystems.disks.remote.host');
         $user = config('filesystems.disks.remote.username');
         $privKey = config('filesystems.disks.remote.privateKey');
+        $root = config('filesystems.disks.remote.root');
         $rsa = new RSA;
         $rsa->loadKey($privKey . '.pub');
 
+        dd($rsa, $privKey . '.pub');
         $ssh = new SSH2($host, 22);
         dump($ssh->getServerPublicHostKey());
         $key = new RSA();
         $key->loadKey(file_get_contents($privKey));
         if (!$ssh->login($user, $key)) {
-            exit('Login Failed');
+            exit('SSH Login Failed');
         }
         dump($ssh->exec('ls -la'));
+
+        $local = __DIR__ . '/../fixtures/images/sampleVig.jpg';
+        $remote = "{$root}/{$this->destFolder}/testVig.jpg";
+
+        dump(
+            $local,
+            $remote,
+            $root,
+            $this->destFolder,
+        );
+        $sftp = new SFTP($host);
+        if (!$sftp->login($user, $key)) {
+            exit('SFTP Login Failed');
+        }
+
+        $sftp->chdir($root);
+        $sftp->mkdir($this->destFolder);
+
+        $sftp->put($remote, $local, SFTP::SOURCE_LOCAL_FILE);
 
         /* if ($expected != $ssh->getServerPublicHostKey()) {
             throw new \Exception('Host key verification failed');
