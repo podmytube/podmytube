d541b860144531d2d91fe9971356557b9974e8b5
frederick tyteca <frederick@tyteca.net>
2021-03-31 19:27:53 +0200

    generating podcast in /tmp

--- app/Factories/UploadPodcastFactory.php
+++ app/Factories/UploadPodcastFactory.php
@@ -2,11 +2,11 @@
 
 namespace App\Factories;
 
+use App\Exceptions\PodcastSavingFailureException;
 use App\Interfaces\Podcastable;
 use App\Jobs\SendFileBySFTP;
 use App\Podcast\PodcastBuilder;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 
 class UploadPodcastFactory
 {
@@ -29,20 +29,26 @@ class UploadPodcastFactory
         /** getting rendered podcast */
         $renderedPodcast = PodcastBuilder::create($this->podcastable->toPodcast())->render();
 
-        /** saving it in tmp */
-        Storage::disk('tmp')->put($this->podcastable->relativeFeedPath(), $renderedPodcast);
+        /** saving it in /tmp */
+        $localPath = "/tmp/{$this->podcastable->relativeFeedPath()}";
+        $dirname = pathinfo($localPath, PATHINFO_DIRNAME);
+        if (!is_dir($dirname)) {
+            if (!mkdir($dirname)) {
+                throw new PodcastSavingFailureException("mkdir {$dirname} has failed, cannot save it locally.");
+            }
+        }
+
+        $status = file_put_contents($localPath, $renderedPodcast);
+        if ($status === false) {
+            throw new PodcastSavingFailureException("Saving rendered podcast to {$localPath} has failed.");
+        }
 
         /** uploading */
-        SendFileBySFTP::dispatchNow($this->localPath(), $this->remotePath(), $cleanAfter = true);
+        SendFileBySFTP::dispatchNow($localPath, $this->remotePath(), $cleanAfter = true);
 
         Log::debug("Podcast {$podcastable->podcastTitle()} has been successfully updated.");
         Log::debug("You can check it here : {$podcastable->podcastUrl()}");
         return $this;
-    }
-
-    public function localPath(): string
-    {
-        return Storage::disk('tmp')->path($this->podcastable->relativeFeedPath());
     }
 
     public function remotePath(): string
