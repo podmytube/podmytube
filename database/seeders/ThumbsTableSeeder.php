4898470cf5fa79a1c9d77ec558bd374166ee2e2c
frederick tyteca <frederick@tyteca.net>
2021-06-21 09:25:57 +0200

    wip: seeder for thumb + vig

---
+++ database/seeders/ThumbsTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Channel;
+use App\Modules\Vignette;
+use App\Thumb;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class ThumbsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        if (!App::environment('local')) {
+            return false;
+        }
+
+        DB::table('thumbs')->delete();
+
+        /** getting channel */
+        $channel = Channel::byChannelId(ChannelsTableSeeder::JEANVIET_CHANNEL_ID);
+
+        $filename = 'jeanviet.jpg';
+        $filepath = $channel->channelId() . DIRECTORY_SEPARATOR . $filename;
+
+        /**
+         * copying lorem image to its new fake location to be tested
+         */
+        Storage::disk(Thumb::LOCAL_STORAGE_DISK)->put(
+            $filepath,
+            file_get_contents(base_path('tests/fixtures/images/jeanviet.jpg'))
+        ) ;
+        $filesize = Storage::disk(Thumb::LOCAL_STORAGE_DISK)->size($filepath);
+
+        /** create thumb */
+        $thumb = Thumb::create([
+            'file_name' => $filename,
+            'file_disk' => Thumb::LOCAL_STORAGE_DISK,
+            'file_size' => $filesize,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'coverable_type' => get_class($channel),
+            'coverable_id' => $channel->channelId(),
+        ]);
+
+        Vignette::fromThumb($thumb)->makeIt()->saveLocally();
+    }
+}
