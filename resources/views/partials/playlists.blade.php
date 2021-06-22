83103f6b4b8f7ef7b2cc92d5a7de9fa8d541e9e9
frederick tyteca <frederick@tyteca.net>
2021-06-22 21:36:18 +0200

    svg are now extracted from blade views and included with @php directives

--- resources/views/partials/playlists.blade.php
+++ resources/views/partials/playlists.blade.php
@@ -1,16 +1,15 @@
-
-@if($playlists->count())
-<h2 class="text-3xl md:text-5xl text-white font-semibold my-3">
-    Your playlists
-</h2>
+@if ($playlists->count())
+    <h2 class="text-3xl md:text-5xl text-white font-semibold my-3">
+        Your playlists
+    </h2>
 
     <ul>
-    @foreach ($playlists as $playlist)
-        <li>
-            {{ $playlist->title }} 
-            <a href="{{ route('playlist.cover.edit', $playlist) }}" class="underline">Update cover</a> 
-            @if ($playlist->active) ✅ @endif
-        </li>
-    @endforeach
+        @foreach ($playlists as $playlist)
+            <li>
+                {{ $playlist->title }}
+                <a href="{{ route('playlist.cover.edit', $playlist) }}" class="underline">Update cover</a>
+                @if ($playlist->active) ✅ @endif
+            </li>
+        @endforeach
     </ul>
 @endif
