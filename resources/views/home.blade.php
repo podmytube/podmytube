d78a58331eb22cc9eb9bf7899393b917a9575c52
frederick tyteca <frederick@tyteca.net>
2021-06-22 21:36:18 +0200

    svg are now extracted from blade views and included with @php directives

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.app')
 
-@section('pageTitle', __('messages.page_title_home_index') )
+@section('pageTitle', __('messages.page_title_home_index'))
 
 
 @section('content')
 
-<div class="max-w-screen-xl mx-auto text-gray-100 py-12 px-4">
-	@include ('partials.channels')
-	@include ('partials.playlists')
-</div>
-<!--/home main container-->
+    <div class="max-w-screen-xl mx-auto text-gray-100 py-12 px-4">
+        <div class="flex flex-col sm:flex-row">
+            <div>
+                @include ('partials.channels')
+            </div>
+            <div>
+                @include ('partials.playlists')
+            </div>
+        </div>
+    </div>
+    <!--/home main container-->
 @endsection
