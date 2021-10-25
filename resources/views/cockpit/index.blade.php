270a51451e048fdd67bed9106313325a69e0d678
frederick tyteca <frederick@tyteca.net>
2021-10-25 21:33:06 +0200

    small improvement on cockpit

--- resources/views/cockpit/index.blade.php
+++ resources/views/cockpit/index.blade.php
@@ -4,10 +4,24 @@
 
 @section('content')
 
+    <div class="max-w-screen-xl mx-auto text-gray-100 py-12 px-4">
+        <div class="flex flex-col sm:flex-row sm:items-start">
+            <div class="p-4 rounded border border-grey-dark">
+                <h4 class="h4">last registered channel</h4>
+                <span class="h3">{{ $lastRegisteredChannel->title() }}</span>
+                ({{ $lastRegisteredChannel->subscription->plan->name }})
+                <p>
+                    <a href="{{ $lastRegisteredChannel->podcastUrl() }}">podcast</a>
+                    <a href="{{ $lastRegisteredChannel->youtubeUrl() }}">channel</a>
+                </p>
+            </div>
 
-    <h2>last registered channel</h2>
-    <h3>{{ $lastRegisteredChannel->title() }}</h3>
-    <a href="{{ $lastRegisteredChannel->podcastUrl() }}">podcast</a>
-    <a href="{{ $lastRegisteredChannel->youtubeUrl() }}">channel</a>
+            <div class="m-2 p-4 rounded border border-grey-dark">
+                <h3 class="h3">nb active podcasts</h3>
+                <p>{{ $nbActiveChannels }}</p>
+            </div>
+        </div>
+    </div>
+
 
 @endsection
