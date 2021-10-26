f4f1cd4390931a9c1113eb094fd901c87fa8d9c9
frederick tyteca <frederick@tyteca.net>
2021-10-26 11:34:35 +0200

    first release of cockpit

--- resources/views/cockpit/index.blade.php
+++ resources/views/cockpit/index.blade.php
@@ -4,21 +4,33 @@
 
 @section('content')
 
-    <div class="max-w-screen-xl mx-auto text-gray-100 py-12 px-4">
-        <div class="flex flex-col sm:flex-row sm:items-start">
-            <div class="p-4 rounded border border-grey-dark">
-                <h4 class="h4">last registered channel</h4>
-                <span class="h3">{{ $lastRegisteredChannel->title() }}</span>
-                ({{ $lastRegisteredChannel->subscription->plan->name }})
-                <p>
-                    <a href="{{ $lastRegisteredChannel->podcastUrl() }}">podcast</a>
-                    <a href="{{ $lastRegisteredChannel->youtubeUrl() }}">channel</a>
-                </p>
+    <div class="max-w-screen-xl mx-auto mt-4 mb-16">
+        <div class="flex flex-col sm:flex-row sm:flex-wrap sm:justify-start">
+            @if ($lastRegisteredChannel)
+                <div class="mt-2 mx-2 py-2 px-4 rounded border bg-gray-800 text-gray-100">
+                    <h4 class="h4">last channel</h4>
+                    <span class="h3">{{ $lastRegisteredChannel->title() }}</span>
+                    ({{ $lastRegisteredChannel->subscription->plan->name }})
+                    <p class="text-center">
+                        <a href="{{ $lastRegisteredChannel->podcastUrl() }}">podcast</a>
+                        <a href="{{ $lastRegisteredChannel->youtubeUrl() }}">channel</a>
+                    </p>
+                </div>
+            @endif
+
+            <div class="mt-2 mx-2 py-2 px-4 rounded border bg-gray-800 text-gray-100">
+                <h3 class="h3">nb podcasts</h3>
+                <p class="text-center">{{ $nbActiveChannels }}</p>
             </div>
 
-            <div class="m-2 p-4 rounded border border-grey-dark">
-                <h3 class="h3">nb active podcasts</h3>
-                <p>{{ $nbActiveChannels }}</p>
+            <div class="mt-2 mx-2 py-2 px-4 rounded border bg-gray-800 text-gray-100">
+                <h3 class="h3">nb medias</h3>
+                <p class="text-center">{{ $nbMedias }}</p>
+            </div>
+
+            <div class="mt-2 mx-2 py-2 px-4 rounded border bg-gray-800 text-gray-100">
+                <h3 class="h3">revenues</h3>
+                <p class="text-center">{{ $revenues }} &euro;</p>
             </div>
         </div>
     </div>
