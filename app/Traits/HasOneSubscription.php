be6db55cb7e52f57ff1f1eac34d6eac40ff79e50
frederick tyteca <frederick@tyteca.net>
2022-01-06 11:19:39 +0100

    display upgrade button if no active subscription

--- app/Traits/HasOneSubscription.php
+++ app/Traits/HasOneSubscription.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Traits;
 
 use App\Subscription;
@@ -9,5 +11,10 @@ trait HasOneSubscription
     public function subscription()
     {
         return $this->hasOne(Subscription::class, 'channel_id');
+    }
+
+    public function hasSubscription(): bool
+    {
+        return $this->subscription !== null;
     }
 }
