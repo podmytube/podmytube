45c059d117fd4aa51c62d3f90bc9851f6b4dc5d9
frederick tyteca <frederick@tyteca.net>
2022-08-07 15:30:48 +0200

    upgrading spatie\laravel-stripe-webhooks need one migration and config update

--- config/stripe-webhooks.php
+++ config/stripe-webhooks.php
@@ -12,6 +12,13 @@ return [
     'signing_secret' => env('STRIPE_WEBHOOK_SECRET'),
 
     /*
+     * You can define a default job that should be run for all other Stripe event type
+     * without a job defined in next configuration.
+     * You may leave it empty to store the job in database but without processing it.
+     */
+    'default_job' => '',
+
+    /*
     * You can define the job that should be run when a certain webhook hits your application
     * here. The key is the name of the Stripe event type with the `.` replaced by a `_`.
     *
@@ -28,5 +35,14 @@ return [
     * The classname of the model to be used. The class should equal or extend
     * Spatie\StripeWebhooks\ProcessStripeWebhookJob.
     */
-    'model' => \Spatie\StripeWebhooks\ProcessStripeWebhookJob::class,
+    'model' => \Spatie\WebhookClient\Models\WebhookCall::class,
+
+    // This class determines if the webhook call should be stored and processed.
+    'profile' => \Spatie\StripeWebhooks\StripeWebhookProfile::class,
+
+    /*
+     * When disabled, the package will not verify if the signature is valid.
+     * This can be handy in local environments.
+     */
+    'verify_signature' => env('STRIPE_SIGNATURE_VERIFY', true),
 ];
