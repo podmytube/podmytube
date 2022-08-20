c6a7da898685b4d9ead1e1877c5754c60001b2a9
frederick tyteca <frederick@tyteca.net>
2022-08-20 16:02:03 +0200

    showable but not really nice

--- resources/views/analytics/show.blade.php
+++ resources/views/analytics/show.blade.php
@@ -6,21 +6,28 @@
 
     <div class="max-w-screen-xl mx-auto py-6 md:py-12 px-4">
         <h2 class="text-3xl md:text-5xl text-white font-semibold">📈 Analytics ⚡</h2>
-
-        <p class="bg-gray-100">
-            <a href="{{ route('analytics', $channel) }}?p={{ App\Http\Controllers\AnalyticsController::PERIOD_THIS_MONTH }}">
-                this month
-            </a>
-            <a href="{{ route('analytics', $channel) }}?p={{ App\Http\Controllers\AnalyticsController::PERIOD_THIS_WEEK }}">
-                this week
-            </a>
-            <a href="{{ route('analytics', $channel) }}?p={{ App\Http\Controllers\AnalyticsController::PERIOD_LAST_MONTH }}">
-                last month
-            </a>
-            <a href="{{ route('analytics', $channel) }}?p={{ App\Http\Controllers\AnalyticsController::PERIOD_LAST_WEEK }}">
-                this week
-            </a>
-        </p>
+        <ul class="bg-gray-100">
+            <li>
+                <a
+                    href="{{ route('analytics', $channel) }}?p={{ App\Http\Controllers\AnalyticsController::PERIOD_THIS_MONTH }}">
+                    this month</a>
+            </li>
+            <li>
+                <a
+                    href="{{ route('analytics', $channel) }}?p={{ App\Http\Controllers\AnalyticsController::PERIOD_THIS_WEEK }}">
+                    this week</a>
+            </li>
+            <li>
+                <a
+                    href="{{ route('analytics', $channel) }}?p={{ App\Http\Controllers\AnalyticsController::PERIOD_LAST_MONTH }}">
+                    last month</a>
+            </li>
+            <li>
+                <a
+                    href="{{ route('analytics', $channel) }}?p={{ App\Http\Controllers\AnalyticsController::PERIOD_LAST_WEEK }}">
+                    this week</a>
+            </li>
+        </ul>
 
         <canvas id="analytics"></canvas>
     </div>
@@ -37,26 +44,18 @@
                         datasets: [{
                             label: '# of downloads',
                             data: {!! $ordinate !!},
+                            cubicInterpolationMode: 'monotone',
                             backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                                'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)',
-                                'rgba(255, 159, 64, 0.2)'
+                                'rgba(255, 206, 86, 1)'
                             ],
                             borderColor: [
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)',
-                                'rgba(255, 159, 64, 1)'
+                                'rgba(255, 206, 86, 1)'
                             ],
-                            borderWidth: 1
+                            borderWidth: 3
                         }]
                     },
                     options: {
+                        responsive: true,
                         scales: {
                             y: {
                                 beginAtZero: true
