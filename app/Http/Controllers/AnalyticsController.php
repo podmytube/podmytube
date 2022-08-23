acb99606d1815a0d9c0ff23196eb7c67247046d9
frederick tyteca <frederick@tyteca.net>
2022-08-23 14:47:12 +0200

    cleaning analytics controller

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Channel;
-use App\Models\Download;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -29,23 +28,7 @@ class AnalyticsController extends Controller
     {
         $this->authorize($channel);
 
-        [$startDate, $endDate] = $this->fromPeriodToDates(intval($request->query('p')));
-
-        $downloads = Download::downloadsForChannelByDay($channel, $startDate, $endDate);
-
-        $abscissa = $ordinate = [];
-        while ($startDate->lessThan($endDate)) {
-            $abscissa[] = $startDate->format('j M');
-
-            $result = $downloads->first(fn (Download $download) => $startDate->toDateString() === $download->log_day->toDateString());
-            $ordinate[] = $result !== null ? $result->counted : 0;
-            $startDate->addDay();
-        }
-
-        $abscissa = "['" . implode("','", $abscissa) . "']";
-        $ordinate = "['" . implode("','", $ordinate) . "']";
-
-        return view('analytics.show', compact('channel', 'downloads', 'startDate', 'endDate', 'abscissa', 'ordinate'));
+        return view('analytics.show', compact('channel'));
     }
 
     /**
